861a811969816f0dedc801e72fb13b6b35feff35
unknown <ytonoli@gmail.com>
2020-12-10 22:37:35 -0300

    post e put

---
+++ blogPessoal/src/app/delete-tema/delete-tema.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-tema',
+  template: `
+    <p>
+      delete-tema works!
+    </p>
+  `,
+  styleUrls: ['./delete-tema.component.css']
+})
+export class DeleteTemaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ blogPessoal/src/app/put-tema/put-tema.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-put-tema',
+  template: `
+    <p>
+      put-tema works!
+    </p>
+  `,
+  styleUrls: ['./put-tema.component.css']
+})
+export class PutTemaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
